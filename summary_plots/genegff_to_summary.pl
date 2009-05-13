d9e4b36171c20d73226201e45f25ebc9fb59919b
Jason Stajich <jason.stajich@gmail.com>
2009-05-13 10:37:49 -0700

    dump intergenic data as well

--- summary_plots/genegff_to_summary.pl
+++ summary_plots/genegff_to_summary.pl
@@ -17,11 +17,17 @@ my $dir = shift;
 my $db = Bio::DB::SeqFeature::Store->new(-adaptor => 'berkeleydb',
 					 -dir     => $dir);
 
+open(my $genefh => ">gene_summary.tab")  || die $!;
+open(my $interfh => ">intergenic_summary.tab") || die $!;
 
-print "#", join("\t", qw(gene transcript clone chrom strand pep_length 
+print $genefh "#", join("\t", qw(gene transcript clone chrom strand pep_length 
 			 spliced_length start stop chrom_start chrom_stop
 			 genome_start genome_stop gmap source version method)),
     "\n";
+
+print $interfh "#", join("\t", qw(chrom chrom_start chrom_stop length
+				  left_gene left_strand 
+				  right_gene right_strand)),"\n";
 
 my %CHROMS;
 {
@@ -35,24 +41,54 @@ my $offset = 0;
 for my $chrom ( sort { $CHROMS{$a}->[0] <=> $CHROMS{$b}->[0] }
 		keys %CHROMS ) {
     my $segment = $db->segment($chrom);
-    for my $gene ( sort { $a->start * $b->strand <=> 
-			      $b->start * $b->strand } 
+    my $lastgene;
+    
+    for my $gene ( sort { $a->start <=> $b->start } 
 		   $segment->features('gene') ) {
+	if( ! $lastgene ) {
+	    if( $gene->start > 1 ) {
+		print $interfh join("\t", $gene->seq_id,
+				    1,$gene->start-1,
+				    $gene->start-1,
+				    'CHROM_START','+1', 
+				    $gene->id, 
+				    $gene->strand > 0 ? "+1" : "-1" ),"\n";
+	    }
+	} else {
+	    print $interfh join
+		("\t", $gene->seq_id,
+		 $lastgene->end+1,$gene->start-1,
+		 ($gene->start-1 - $lastgene->end),
+		 $lastgene->id,$lastgene->strand > 0 ? "+1" : "-1",
+		 $gene->id,$gene->strand > 0 ? "+1" : "-1" ),"\n";
+	}
+	$lastgene = $gene;
 	for my $mRNA ( $gene->get_SeqFeatures ) {
 	    my $cds_len = 0;
 	    for my $cds ( $mRNA->get_SeqFeatures('cds') ) {
 
 		$cds_len += $cds->length;
 	    }
-	    print join("\t", $gene->id, $mRNA->id, 
-		       $mRNA->seq_id, $mRNA->seq_id,
-		       $gene->strand > 0 ? "+1" : "-1", ($cds_len/3) -1,$cds_len,$gene->start, 
-		       $gene->end, $gene->start,$gene->end, 
-		       $offset + $gene->start,$offset + $gene->end, 
-		       0, $gene->source_tag, $version, $method),"\n";
+	    print $genefh join("\t", $gene->id, $mRNA->id, 
+			       $mRNA->seq_id, $mRNA->seq_id,
+			       $gene->strand > 0 ? "+1" : "-1", ($cds_len/3) -1,
+			       $cds_len,$gene->start, 
+			       $gene->end, $gene->start,$gene->end, 
+			       $offset + $gene->start,$offset + $gene->end, 
+			       0, $gene->source_tag, $version, $method),"\n";
 	}
 	last if $debug;
     }
     last if $debug;
     $offset += $CHROMS{$chrom}->[1];
+    if( $lastgene ) {
+	if( $lastgene->end < $CHROMS{$chrom}->[1]) {
+	    print $interfh join
+		("\t", $lastgene->seq_id, 
+		 $lastgene->end+1,$CHROMS{$chrom}->[1], 
+		 ($CHROMS{$chrom}->[1] - $lastgene->end),
+		 $lastgene->id, $lastgene->strand > 0 ? "+1" : "-1",
+		 'CHROM_END','+1'),"\n";
+	}
+    }
 }
