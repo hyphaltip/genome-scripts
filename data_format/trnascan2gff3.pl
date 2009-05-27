64edee663b5ecaf5065c0847fc0fb259a81d0b3e
Jason Stajich <jason.stajich@gmail.com>
2009-05-27 14:33:52 -0700

    exon and gene are the feature types we want

--- data_format/trnascan2gff3.pl
+++ data_format/trnascan2gff3.pl
@@ -8,9 +8,14 @@ my $parser = Bio::Tools::tRNAscanSE->new(-file => shift);
 
 # parse the results
 while( my $gene = $parser->next_prediction ) {
-     $out->write_feature($gene);
+    my ($id) = $gene->get_tag_values('ID');
+    $gene->primary_tag('gene');
+    $out->write_feature($gene);
+    my $i = 0;
      for my $trans ( $gene->get_SeqFeatures() ) {	
+	 $trans->primary_tag('exon');
 	 $out->write_feature($trans,$trans->get_SeqFeatures());
+	 $i++;
     }
 
 }
