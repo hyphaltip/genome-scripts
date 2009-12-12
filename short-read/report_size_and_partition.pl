88a77e4a166782f3dca2d57e6392370ed3393099
Jason Stajich <jstajich@puffball.fungalgenomes.org>
2009-12-12 11:09:54 -0800

    report 5' and 3' base frequency

--- short-read/report_size_and_partition.pl
+++ short-read/report_size_and_partition.pl
@@ -2,6 +2,8 @@
 use strict;
 use Getopt::Long;
 use File::Spec;
+use List::Util qw(sum);
+
 my %expected_bases = map { $_ => 1 } qw(C A G T);
 my %compress = ('bz2' => 'bzcat',
 		'gz'  => 'zcat',
@@ -37,15 +39,41 @@ for my $file ( @ARGV ) {
 	}
 	print $ofh $_,"\n";
 	my $five_base = uc substr($seq,0,1); # get 5' base
+	my $three_base = uc substr($seq,-1,1); # get 3' base
 	next unless $expected_bases{$five_base};
-	$counts{$length}->{$five_base}++;
+	$counts{$length}->{5}->{$five_base}++;
+
+	next unless $expected_bases{$three_base};
+	$counts{$length}->{3}->{$three_base}++;
     }
 
-    open(my $rpt => ">$dir/$base.summary_sizes" ) || die $!;
+    open(my $rpt => ">$dir/$base.5_summary_sizes" ) || die $!;
+    open(my $rptpct => ">$dir/$base.5_summary_sizes.percent" ) || die $!;
     my @lengths = sort { $a <=> $b } keys %counts;
     my @bases   = sort keys %expected_bases;
-    print $rpt join("\t", qw(LENGTH), @bases),"\n";
+    print $rpt join("\t", qw(LENGTH),  @bases),"\n";
+    print $rptpct join("\t", qw(LENGTH), @bases),"\n";
     for my $c ( @lengths ) {
-	print $rpt join("\t", $c, map { $counts{$c}->{$_} } @bases), "\n";
+	print $rpt join("\t", $c, map { $counts{$c}->{5}->{$_} } @bases), "\n";
+	my $sum = sum ( map { $counts{$c}->{5}->{$_} } @bases );
+	print $rptpct join("\t", $c, map { sprintf("%.2f",100*$counts{$c}->{5}->{$_}/$sum) } @bases),
+	"\n";
     }
+    close($rpt);
+    close($rptpct);
+    open($rpt => ">$dir/$base.3_summary_sizes" ) || die $!;
+    open($rptpct => ">$dir/$base.3_summary_sizes.percent" ) || die $!;
+    @lengths = sort { $a <=> $b } keys %counts;
+    @bases   = sort keys %expected_bases;
+    print $rpt join("\t", qw(LENGTH),  @bases),"\n";
+    print $rptpct join("\t", qw(LENGTH), @bases),"\n";
+
+    for my $c ( @lengths ) {
+	print $rpt join("\t", $c, map { $counts{$c}->{3}->{$_} } @bases), "\n";
+	my $sum = sum ( map { $counts{$c}->{5}->{$_} } @bases );
+	print $rptpct join("\t", $c, map { sprintf("%.2f",100*$counts{$c}->{3}->{$_}/$sum) } @bases),
+	"\n";
+    }
+
 }
+
