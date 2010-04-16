35dc89eb4204b252468c638678c45ec302bf0163
Jason Stajich <jstajich@puffball.ucr.edu>
2010-04-15 17:29:56 -0700

    minor

--- R-gists/summary_shortread_barplot.R
+++ R-gists/summary_shortread_barplot.R
@@ -1,6 +1,6 @@
 size <- read.table("%INPUT%",header=T,sep="\t",row.names=1)
 
 pdf("%INPUT%.pdf")
-barplot(t(size),xlab="Read Length", ylab="Total # Reads", main="Reads mapped by size",space=0.1,cex.axis=0.8,las=1,cex=0.8,names=size$V1,legend=T,col=rainbow(5, start=.1, end=.91),beside=F))
+barplot(t(size),xlab="Read Length", ylab="Total # Reads", main="Reads mapped by size",space=0.1,cex.axis=0.8,las=1,cex=0.8,names=size$V1,legend=T,col=rainbow(5, start=.1, end=.91),beside=F)
 
 
---
+++ short-read/scan_for_siRNA_SAM.pl
@@ -0,0 +1,4 @@
+#!/usr/bin/perl -w
+use strict;
+use Bio::DB::Sam;
+
