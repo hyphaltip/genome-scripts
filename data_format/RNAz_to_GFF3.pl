af8298b291cd1ec89e0b6e7956f1c111a536336f
Jason Stajich <jason@bioperl.org>
2010-06-05 17:26:37 -0700

    convert RNAz to GFF3

--- data_format/RNAz_to_GFF3.pl
+++ data_format/RNAz_to_GFF3.pl
@@ -2,9 +2,10 @@
 use strict;
 use Getopt::Long;
 my $dir =  ".";
-
+my $master_ref = 'Nc10';
 GetOptions(
 	   'd|dir:s' => \$dir,
+	   'r|ref:s' => \$master_ref,
 	   );
 
 my $pattern = qr/(\S+)_(\d+)-(\d+)\.RNAz/;
@@ -12,7 +13,7 @@ my $exp_status = 'RNA';
 print "##gff-version 3\n";
 opendir(DIR, $dir) || die $!;
 my $rnazid = 0;
-my $ref;
+my $ref = $master_ref;
 for my $file ( readdir(DIR) ) {
 
     if( $file =~ $pattern ) {
@@ -26,16 +27,22 @@ for my $file ( readdir(DIR) ) {
 	# >Lbic
 	# >Scom 
 	# etc
-	my ($ref) = split(/_/,$chrom);
+	#my ($ref) = split(/_/,$chrom);
 #	warn("$file $chrom:$start..$stop\n");
 	open(my $fh => "$dir/$file") || die $!;
-	my (%dat,$seen_hash,%seqs,%struct);
+	my (%dat,$seen_hash,%seqs,%struct,@dats);
+	my $pref_strand;
+	my $i = -1;
 	while(<$fh>) {
-	    if( /\s*(.+):\s+(\S+)/ ) { # matcing the NAME: DATA for RNAz output
-		$dat{$1} = $2;		
+	    if( /^#\s+Strand winner:\s+(\S+)/ ) {
+	  	$pref_strand = $1;
+                %dat = ( $pref_strand eq 'forward' ) ? %{$dats[0]} : %{$dats[1]};
+	    } elsif( /\s*(.+):\s+(\S+)/ ) { # matcing the NAME: DATA for RNAz output
+		$dats[$i]->{$1} = $2;
 #		warn("$1 --> $2\n");
 	    } elsif( /^\#+\s+RNAz\s+(\S+)\s+\#+/ ) {
 		# skip
+		$dats[++$i]->{'Version'} = 'RNAz_2.0';
 	    } elsif( /^\#+\s+$/ ) {
 		$seen_hash = 1;
 	    } elsif( $seen_hash ) {
@@ -44,16 +51,16 @@ for my $file ( readdir(DIR) ) {
 		    $seqs{$seqid} = <$fh>;
 		    my $tmp;
 		    ($struct{$seqid},$tmp) = split(/\s+/,<$fh>,2);
-		    if( $tmp =~ /\(\s*(-?\d+\.\d+)/) {			
-			($dat{'MFE'}->{$seqid}) = $1;
+		    if( $tmp =~ /\(\s*(-?\d+\.\d+),/) {
+			($dats[$i]->{'MFE'}->{$seqid}) = $1;
 		    } 
 		}
-	    }
+	    } 
 	}
 	close($fh);
-	$dat{'Reading direction'} = 
-	    $dat{'Reading direction'} eq 'forward' ? '+' : '-';
-	if( $exp_status eq $dat{'Prediction'} ) {
+	if( $pref_strand && $exp_status eq $dat{'Prediction'} ) {
+  	    $dat{'Reading direction'} = 
+	        $dat{'Reading direction'} eq 'forward' ? '+' : '-';
 	    print join("\t",
 		       $chrom,'RNAz','ncRNA',$start,$stop,
 		       $dat{'SVM RNA-class probability'},
@@ -65,8 +72,6 @@ for my $file ( readdir(DIR) ) {
 			       $struct{$ref},
 			       )),"\n";
 	    $rnazid++;
-	
 	}
-
-    }
+     }
 }
