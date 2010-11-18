1e6d8ced49571c5b4b7f36f98ab18fe1ea4d0aae
Jason Stajich <jason@bioperl.org>
2010-11-17 16:05:43 -0800

    collapse these by phylogenetic clades

--- comparative/orthomcl_phylogenetic_profile.pl
+++ comparative/orthomcl_phylogenetic_profile.pl
@@ -11,7 +11,7 @@ my $pfamdir;
 my $pfamext = 'tab';
 my $hmmerversion = 3;
 my $evalue_cutoff = 0.01;
-my %clade_names = ('Metazoa' => [qw(Hsap Mbre ;
+my @collapse;
 GetOptions(
 	   'd|p|pfam:s'  => \$pfamdir,
 	   'ext:s'     => \$pfamext,
@@ -19,14 +19,14 @@ GetOptions(
 	   'e|evalue:s' => \$evalue_cutoff,
 	   'i|input:s'  => \$input,
 	   'o|output:s' => \$odir,
-
+	   'c|collapse:s{2}' => \@collapse,
 	   );
+my %collapse = @collapse;
 
 $input ||= shift @ARGV;
 $odir ||= $input."-profile";
 
 die "must provide a folder with pfam table data (either domtblout or hmmer2table output\n" unless $pfamdir && -d $pfamdir;
-
 my %pfam;
 opendir(PFAM, $pfamdir) || die "cannot open $pfamdir: $!";
 for my $file ( readdir(PFAM) ) {
@@ -52,7 +52,7 @@ for my $file ( readdir(PFAM) ) {
 	warn("unknown HMMER version (2 or 3 are expected)\n");
     }
 }
-
+open(my $genefh =>">$odir.pergene.tab");
 open(my $fh => $input) || die "cannot open $input: $!\n";
 my (%sp,%group2name,%clade);
 while(<$fh>) {   
@@ -62,14 +62,18 @@ while(<$fh>) {
     my %domains;	
     for my $gene ( @orthologs ) {
 	my ($sp)= split(/\|/,$gene);
+	if( $collapse{$sp} ) {
+	    $sp = $collapse{$sp};
+	}
 	$count{$sp}++;
 	$sp{$sp} = 1; # collecting all the species names
 	push @{$group2name{$group}}, $gene;
-    }    
+    }
     my $nm = join("-", sort keys %count);
     push @{$clade{$nm}}, $group;
 }
 mkdir($odir) unless -d $odir;
+print $genefh join("\t",qw(GENE CLADE ORTHOGROUP DOMAINS)),"\n"; 
 while( my ($clade,$og) = each %clade ) {
     open(my $ofh => ">$odir/$clade.phyloprofile.tab") || die $!;
     print $ofh join("\t", 'ORTHOGROUP','GENE', 'DOMAINS'), "\n";
@@ -77,6 +81,7 @@ while( my ($clade,$og) = each %clade ) {
 	for my $gene ( sort @{$group2name{$ortho}} ) {	    
 	    print $ofh join("\t", $ortho, $gene, 
 			    join(",", keys %{$pfam{$gene} || {}})),"\n";
+	    print $genefh join("\t", $gene, $clade, $ortho, join(",", keys %{$pfam{$gene} || {}})),"\n";
 	}
     }
 }
