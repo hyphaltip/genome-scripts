496d53cf44e2f69a1c6c82ea341a2db3b7ce66c8
Jason Stajich <jason@bioperl.org>
2010-12-06 09:44:56 -0800

    simple start..stop extract of seqfeatures, will look at adding the spliced seq info at some point but that is implemented in other scripts

---
+++ gff_tools/gff3_extract_seq.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/perl -w
+use strict;
+use Bio::DB::Fasta;
+use Bio::SeqIO;
+use Getopt::Long;
+my ($db,$output);
+GetOptions(
+	   'd|db:s'     => \$db,
+	   'o|output:s' => \$output,
+	   );
+# simple start-stop features to seq
+my $dbh = Bio::DB::Fasta->new($db || shift);
+my $out;
+if( $output) {
+#    $out = Bio::SeqIO->new(-format => 'fasta',
+#			   -file => ">$output");
+    open($out => ">$output") || die "cannot open $output: $!\n";
+} else {
+#    $out = Bio::SeqIO->new(-format => 'fasta');
+    $out = \*STDOUT;
+}
+while(<>) {
+    next if /^\#/;
+    chomp;
+    my @row = split(/\t/,$_);
+    my ($start,$end) = sort { $a <=> $b } ($row[3],$row[4]);
+    ($start,$end) = ($end,$start) if $row[6] eq '-' || $row[6] eq '-1';
+    my $seq = $dbh->seq($row[0], $start => $end );
+    my ($name);
+    if($row[-1] =~ /(ID|Parent|Gene)=([^;]+)/) {
+	$name = $2;
+    } else {
+	$name = sprintf("%s_%d-%d",$row[0],$start,$end);
+    }
+#    $out->write_seq(Bio::Seq->new(-id => $name,
+#				  -seq => $seq,
+#				  -desc => sprintf("%s:%d..%d",
+#						   $row[0],$start,$end)));
+    printf $out ">%s %s:%d..%d length=%d\n%s\n",$name,$row[0],$start,$end,length($seq),$seq;
+}
