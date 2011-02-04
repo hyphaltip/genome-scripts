5b7ad4925c31ea480fea3064dd043cffa5be54db
Jason Stajich <jason@bioperl.org>
2011-02-03 23:50:47 -0800

    commenting the code

--- conservation_profile/MSA_scan_for_large_indels.pl
+++ conservation_profile/MSA_scan_for_large_indels.pl
@@ -18,6 +18,7 @@ GetOptions('r|ref=s' => \$genome,
 die"must have genome with -r or --ref\n" unless $genome;
 $agp_ref ||= "$genome.agp";
 
+# read in the AGP file from Mercator to get the start/stop of chromosomes
 open(my $agp => $agp_ref ) || die "$agp_ref: $!";
 my %refchroms;
 while(<$agp>) {
@@ -28,49 +29,55 @@ while(<$agp>) {
 for my $chrom ( sort keys %refchroms ) { 
     my ($start,$end)= @{$refchroms{$chrom}};
     warn("$chrom $start .. $end\n");
+ # run sliceAlignment to get the projected MSA
     open( my $fh => sprintf($exe, $alndir,$genome,$chrom,
 			    $start,$end,$strand)) || 
 				die "$!";    
 
     my $alnio = Bio::AlignIO->new(-fh => $fh,
 				  -format   => 'fasta');
-
+   # parse alignment
     while( my $aln = $alnio->next_aln ) {
 	warn("got aln, len ", $aln->length,"\n");
 	my %snv;
 	my @s = $aln->each_seq;
 	my @d =  map { $_->seq } @s;
+	# extract the chromosome:start-stopSTRAND from the description field
 	my @desc = map { my ($chm,$loc) = split(/:/,$_->description);
 			 my ($s,$e,$strd) = ($loc =~ /(\d+)-(\d+)([+-])/);
 			 [($chm,$s,$e,$strd)];
 		     } @s;
 	my $len = $aln->length;
+	# walk down the length of the alignment
 	for( my $i = 0; $i < $len; $i++ ) {
 	    my %alleles;	    
 	    for my $s ( @d ) {
+	        # save time by just getting 1st character, deleting it, but saving it, there are no seeks long string this way
 		my $ch = substr($s,0,1,'');
 		$alleles{$ch}++;
 	    }
-	    next if keys %alleles == 1;
+	    next if keys %alleles == 1; # ignore where monomorphic
 	    if( exists $alleles{'-'} ) {
-		$snv{$i} = 'GAP';
+		$snv{$i} = 'GAP'; # code GAPs
 	    } else { 
-		$snv{$i} = join(",",keys %alleles);
+		$snv{$i} = join(",",keys %alleles); # store the SNP alleles
 	    }
 	}
 	my @gaps = sort { $a <=> $b } grep { $snv{$_} eq 'GAP' } keys %snv;
 	my @snps = sort { $a <=> $b } grep { $snv{$_} ne 'GAP' } keys %snv;
-	my @collapse_gaps = &collapse_nums(@gaps);
+	my @collapse_gaps = &collapse_nums(@gaps); # run the collapse algorithm to get runs of GAPs
 	
 	print "there are ", scalar @snps, " SNPs and ", scalar @gaps, " total gaps and ", scalar @collapse_gaps, " gap openings\n";	
 
 	for my $indel ( @collapse_gaps ) {
-	    if( $indel =~ /(\d+)-(\d+)/ ) {
+	    # capture the indels, check if they are right size, and project the location from 
+	    if( $indel =~ /(\d+)-(\d+)/ ) { # only those non singleton indels
 		my ($from,$to) = ($1,$2);
-		my ($locfrom) = $s[0]->location_from_column($from);
-		my ($locto)  =  $s[0]->location_from_column($to);
 		my ($size_gap) = abs($to - $from);
 		if( $size_gap > $Min_gapsize && $size_gap < $Max_gapsize ) {
+		   my ($locfrom) = $s[0]->location_from_column($from); # project from aln space to seq coords
+		   my ($locto)  =  $s[0]->location_from_column($to); 
+		# Report the info about the gap
 		    printf "%s aln=%s %d..%d %d\n",
 		    $desc[0]->[0],
 		    $indel,		    
