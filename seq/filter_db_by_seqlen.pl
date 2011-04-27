ee50a36339c5515954f2093dd870fcbcbc8ccc99
Jason Stajich <jason@bioperl.org>
2011-04-26 17:52:00 -0700

    explicit STDOUT

--- seq/filter_db_by_seqlen.pl
+++ seq/filter_db_by_seqlen.pl
@@ -5,12 +5,12 @@ use Bio::SeqIO;
 use Getopt::Long;
 my $format = 'fasta';
 my $length = 100;
-GetOptions("f|fasta:s" => \$format,
+GetOptions("f|format:s" => \$format,
 	   'l|len|length:i' => \$length);
 
-my $in = Bio::SeqIO->new(-fh => \*ARGV,
+my $in = Bio::SeqIO->new(-fh    => \*ARGV,
 			 -format=>$format);
-my $out = Bio::SeqIO->new(-format => $format);
+my $out = Bio::SeqIO->new(-fh => \*STDOUT, -format => $format);
 while(my $s = $in->next_seq ){
     next unless $s->length >= $length;
     $out->write_seq($s);
