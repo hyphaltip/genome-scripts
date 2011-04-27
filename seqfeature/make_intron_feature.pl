013c2da8c3273f9481a2f4266087a5b46ec10b94
Jason Stajich <jason@bioperl.org>
2011-04-26 17:51:29 -0700

    John discovered some bugs

--- seqfeature/make_intron_feature.pl
+++ seqfeature/make_intron_feature.pl
@@ -11,29 +11,29 @@ my $src = 'gene:NCBI_PASA';
 my $output;
 
 GetOptions(
-	   'v|verbose!'  => \$debug,
-	   'u|user:s'    => \$user,
-	   'p|pass:s'    => \$pass,
-	   'host:s'      => \$host,
-	   'db|dbname:s' => \$dbname,
-	   
-	   's|src:s'     => \$src,
-	   'o|output:s'  => \$output,
-	   );
+'v|verbose!' => \$debug,
+'u|user:s' => \$user,
+'p|pass:s' => \$pass,
+'host:s' => \$host,
+'db|dbname:s' => \$dbname,
 
-unless(  defined $dbname ) {
+'s|src:s' => \$src,
+'o|output:s' => \$output,
+);
+
+unless( defined $dbname ) {
     die("no dbname provided\n");
 }
 
 ($user,$pass) = &read_cnf($user,$pass) unless $pass && $user;
 my $dsn = sprintf('dbi:mysql:database=%s;host=%s',$dbname,$host);
 my $dbh = Bio::DB::SeqFeature::Store->new(-adaptor => 'DBI::mysql',
-                                          -dsn     => $dsn,
-                                          -user    => $user,
+                                          -dsn => $dsn,
+                                          -user => $user,
                                           -password => $pass,
                                           );
 my $ofh;
-if( $output && $output ne '-' ) { 
+if( $output && $output ne '-' ) {
     open($ofh => ">$output" ) || die $!;
 } else {
     $ofh = \*STDOUT;
@@ -44,6 +44,7 @@ my $iter = $dbh->get_seq_stream(-type => $src);
 my (undef,$from1) = split(/:/,$src);
 my $count = 0;
 my $gene_count = 0;
+
 while( my $gene = $iter->next_seq ) {
     $gene_count++;
     my $gene_name = $gene->name;
@@ -51,31 +52,29 @@ while( my $gene = $iter->next_seq ) {
     if( ! @mRNA ) {
 	warn("no mRNA for $gene_name\n");
     }
-    for my $mRNA ( @mRNA ) { # 1st mRNA for now
+    for my $mRNA ( @mRNA ) {	# 1st mRNA for now
 	my $last_exon;
 	my $i = 1;
 	my $mRNA_name = $mRNA->name || 'mRNA-'.$mRNA->load_id;
-	for my $exon ( sort { $a->start * $a->strand <=> 
-				  $b->start * $b->strand } 
+	for my $exon ( sort { $a->start * $a->strand <=> $b->start * $b->strand }
 		       $mRNA->get_SeqFeatures('exon') ) {
 	    if( $last_exon ) {
-		my ($start,$end) = ( $last_exon->end+1,
-				     $exon->start - 1);
+		my ($start,$end) = ( $last_exon->end+1,$exon->start - 1);
 		if( $exon->strand < 0 ) {
-		    ($start,$end) = ( $last_exon->start-1,
-				      $exon->end + 1);
+		    ($start,$end) = ( $exon->end + 1, $last_exon->start-1);
 		}
+
 		print $ofh join("\t",
 				$gene->seq_id,
 				$gene->source,
 				'intron',
 				$start,$end, '.',
-				$exon->strand > 1 ? '+' : '-',
+				$exon->strand > 0 ? '+' : '-',
 				'.',
 				sprintf('ID=%s.i%d;Gene=%s',
 					$mRNA_name,
 					$i++,
-					$gene_name)),"\n";	    
+					$gene_name)),"\n";
 	    }
 	    $last_exon = $exon;
 	}
@@ -100,3 +99,4 @@ sub read_cnf {
     }
     return ($user,$pass);
 }
+
