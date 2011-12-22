081fb1936045b762ed542366324e429caae655c7
Jason Stajich <jason@bioperl.org>
2011-12-22 14:04:40 -0800

    calc het/var calls

--- popgen/vcftab_snp_counts_rice.pl
+++ popgen/vcftab_snp_counts_rice.pl
@@ -4,6 +4,7 @@ use warnings;
 my $header = <>;
 my (undef,undef,undef,@strains) = split(/\s+/,$header);
 my %counts;
+my %hets;
 while(<>) {
     my ($chr,$pos,$ref,@alleles) = split;
     my $i = 0;
@@ -15,11 +16,55 @@ while(<>) {
 	    $strain{$strain}->{$_}++;
 	    $all_alleles{$_}++;
 	}
+	if( keys %{$strain{$strain}} == 2 ) {
+	    $hets{$strain}++;
+	}
     }
-    next if( exists $all_alleles{'.'} );# should we skip the whole position?
+
+    next if( exists $all_alleles{'.'} ); # should we skip the whole position?
+    
     for my $st ( @strains ) {
-	{}
-    if( ! exists $all_alleles{$ref} ) {
-	$counts{'shared-derived'}++;
+	if( ! defined $strain{$st}->{$ref} ) {
+	    # doesn't have the NB ref allele	    
+	    $counts{'diff_NB-'.$st}++;# += 2; # since neither allele matches add 2
+	} else {
+	    $counts{'shared_NB-'.$st}++;# += $strain{$st}->{$ref};
+	}	
+    }
+    for( my $j =0; $j < @strains; $j++ ) {
+	my $straina = $strains[$j];
+	for( my $k = $j+1; $k < @strains; $k++ ) {
+	    my $strainb = $strains[$k];
+
+	    for my $allele_A (  keys %{$strain{$straina}} ) {
+		if( ! exists $strain{$strainb}->{$allele_A} ) {
+		    $counts{"diff_$straina-$strainb"}++;# += $strain{$straina}->{$allele_A};
+		} else {
+		    $counts{"shared_$straina-$strainb"}++;# += $strain{$straina}->{$allele_A};
+		}
+	    }
+	    for my $allele_B ( keys %{$strain{$strainb}} ) {
+		if( ! exists $strain{$straina}->{$allele_B} ) {
+		    # keep strains in this order so it will be simpler
+		    $counts{"diff_$straina-$strainb"}++;# += $strain{$strainb}->{$allele_B};
+		} 
+		# already counted similarities, would have been in the previous count
+	    }
+	}
     }
 }
+
+for my $strain (sort keys %hets ){
+    print join("\t", 'HET', $strain,$hets{$strain}),"\n";
+}
+
+for my $kind ( grep { /diff/ } sort keys %counts ) {
+    print join("\t", 'DIFF', $kind, '',$counts{$kind} ),"\n";
+}
+
+for my $kind ( grep { /shared/ } sort keys %counts ) {
+    print join("\t", 'SHARED', $kind,'', $counts{$kind} ),"\n";
+}
+
+# what about arabidposis het positions?
+
