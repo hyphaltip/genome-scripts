856285108b84a99f2984f98f4bb5314ef2cb5954
Jason Stajich <jason@bioperl.org>
2012-03-25 12:52:44 -0700

    make the debug option available

--- short-read/filter_casava_N_reads.pl
+++ short-read/filter_casava_N_reads.pl
@@ -1,15 +1,22 @@
 #!/usr/bin/perl -w
 use strict;
+use Getopt::Long;
 
-open(my $skipfh => ">skip") || die $!;
+my $write_skip;
+GetOptions(
+	   'debug|skip!' => \$write_skip,
+	   );
 
+if( $write_skip  ) {
+    open($skipfh => ">skip") || die $!;
+}
 while(my $id = <>) {
     my $seq = <>;
     my $desc =<>;
     my $qual = <>;
     
     if (($id =~ /\S+\s+\d+\:([YN]):\d+/) && $1 eq 'N') {
-	print $skipfh $id,$seq,$desc,$qual;
+	print $skipfh $id,$seq,$desc,$qual if $write_skip;
 	next;
     }
     
