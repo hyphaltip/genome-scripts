406ac90d88088008ccc32ec89a8769e628de0742
Jason Stajich <jason.stajich@ucr.edu>
2012-11-20 16:18:46 -0800

    show 3rd codon for all 4

--- seq/GC3_codons.pl
+++ seq/GC3_codons.pl
@@ -9,6 +9,9 @@ my $pergene;
 
 GetOptions('g|pergene!' => \$pergene);
 
+if( $pergene ) {
+ print join("\t",qw(SEQ GC G C)), "\n";
+}
 my $infile = shift;
 
 my $in = Bio::SeqIO->new(-format => 'fasta', -file => $infile);
@@ -33,7 +36,7 @@ while ( my ($aa,$codons) = each %aa ) {
     }
   }
 }
-my %gc;
+my (%gc,%nongc);
 while (my $seq = $in->next_seq ) {
   my $str = $seq->seq;
   if ( ($seq->length % 3) != 0 ) {
@@ -42,25 +45,43 @@ while (my $seq = $in->next_seq ) {
   } else {
 #    warn($seq->id, " ok\n");
   }
-  my %gene_count;
+  my (%gene_count,%nongene_count);
   while ( $str )  {
    my $codon = substr($str,0,3,'');
+   my $thirdbase = uc substr($codon,2,1);
    if ( $degenerate{$codon} ) {
-     my $synon = uc substr($codon,2,1);
-#     warn("$synon for $codon\n");
-     $gc{$synon}++;
-     $gene_count{$synon}++;
+     $gc{$thirdbase}++;
+     $gene_count{$thirdbase}++;
+   } else {
+     $nongc{$thirdbase}++;
+     $nongene_count{$thirdbase}++;
    }
  }
   next if ! keys %gene_count;
   if ( $pergene ) {
-    printf "%s\t%.3f\n",$seq->id, 
-      ( ($gene_count{'G'} || 0) + ($gene_count{'C'} || 0) ) / 
-	sum(map { $gene_count{$_} || 0} @bases) / 3);
+	my $totalbp = sum(map { $gene_count{$_} || 0} @bases);	
+	my $g = $gene_count{'G'} || 0;
+	my $c = $gene_count{'C'} || 0;
+	my $non_totalbp = sum(map { $nongene_count{$_} || 0} @bases);	
+	my $non_g = $nongene_count{'G'} || 0;
+	my $non_c = $nongene_count{'C'} || 0;
+	
+    printf "%s\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\n",$seq->id, ($g+$c) / $totalbp, $g/$totalbp, $c/$totalbp, 
+	($non_g + $non_c) / $non_totalbp,
+	$non_g / $non_totalbp, $non_c/$non_totalbp;
   }
 }
 
 my $gc = ($gc{'G'} || 0)  + ($gc{'C'} || 0);
-my $total = sum ( map { $gc{$_} } @bases) / 3;
-printf "%.3f %% (%s=%d,%s=%d,%s=%d,%s=%d)\n", $gc / $total,
-  map { $_,$gc{$_} } @bases ;
+my $g = $gc{'G'};
+my $c = $gc{'C'};
+my $total = sum ( map { $gc{$_} } @bases) ;
+print join("\t", qw(CODON_TYPE GC G C)), "\n";
+printf "%s\t%.3f\t%.3f\t%.3f\n",'4+6 fold DG',($g+$c) / $total, $g/$total, $c/$total;
+
+$gc = ($nongc{'G'} || 0)  + ($nongc{'C'} || 0);
+$g = $nongc{'G'};
+$c = $nongc{'C'};
+$total = sum ( map { $nongc{$_} } @bases);
+
+printf "%s\t%.3f\t%.3f\t%.3f\n",'non 4+6 fold DG', ($g+$c) / $total, $g/$total, $c/$total;
