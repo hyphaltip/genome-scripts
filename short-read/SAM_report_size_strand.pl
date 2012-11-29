62cc34265bad7464defa4402637ef411838249f3
Jason Stajich <jason.stajich@ucr.edu>
2012-11-29 14:03:19 -0800

    calculate length from starting sequence not the alignment

--- short-read/SAM_report_size_strand.pl
+++ short-read/SAM_report_size_strand.pl
@@ -66,7 +66,7 @@ for my $file ( @ARGV ) {
 		my %f;
 		for ( split('',$seq) ) { $f{$_}++ }
 		next if keys %f <= 2; # drop those AAA or TTT runs
-		my $length = $align->query->length;
+		my $length = length $seq; # $align->query->length;
 		next if $length < $minsize || $length > $maxsize;
 		my $five_base = uc substr($seq,0,1); # get 5' base
 		my $three_base = uc substr($seq,-1,1); # get 3' base
@@ -88,7 +88,7 @@ for my $file ( @ARGV ) {
 		my %f;
 		for ( split('',$seq) ) { $f{$_}++ }
 		next if keys %f <= 2; # drop those AAA or TTT runs
-		my $length = $align->query->length;
+		my $length = length $seq; #$align->query->length;
 		next if $length < $minsize || $length > $maxsize;
 		my $five_base = uc substr($seq,0,1); # get 5' base
 		my $three_base = uc substr($seq,-1,1); # get 3' base
