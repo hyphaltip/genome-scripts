d6e7118235a35dfd82e42c9fa26b862b28977014
Jason Stajich <jason.stajich@ucr.edu>
2013-05-12 22:36:38 -0700

    downsample by fixed width windows

--- data_format/downsample_bigwig.pl
+++ data_format/downsample_bigwig.pl
@@ -5,30 +5,52 @@ use warnings;
 
 =head1 NAME
 
-condense_bigwig - summarize BEDGraph file to reduced number of windows
+condense_bigwig - summarize BigWig file to reduced number of windows
 
 =head1 SYNOPSIS
 
-condense_bigwig file.bedgraph -window 1000 > file.condense.bedgraph
+condense_bigwig file.bw genome.fa -window 1000 > file.condense.bedgraph
+
+=head1 DESCRIPTION
+
+Used to downsample for Circos drawing
+
+=head1 AUTHOR - Jason Stajich
+
+Jason Stajich, jason.stajich[at]ucr.edu
 
 =cut
 
-use Bio::DB::BigWig 'binMean';;
+use Bio::DB::BigWig 'binMean';
 use Getopt::Long;
 
-my $window = 1000;
+my $windowsize = 1000;
 
 GetOptions(
-	   'w|window:i' => \$window,
+	   'w|windowsize:i' => \$windowsize,
 );
 my ($wigfile,$genome) = @ARGV;
 
 my $wig  = Bio::DB::BigWig->new(-bigwig=>$wigfile,
 				-fasta=>$genome);
+my @seq_ids = $wig->seq_ids;
+for my $sum ( $wig->features(-type=>'summary') ) { # one for each chromosome
+    warn "chromosome ",$sum->seq_id,"\n";
+    my $stats = $sum->statistical_summary($windowsize);
+    warn "\tmax  = ",$stats->[0]{maxVal},"\n";
+    warn "\tmean = ",binMean($stats->[0]),"\n";
+}
 
-# fix this to just get windows of a fixed length?
-my @bins = $wig->features(-type=>"bin:$window"); # same as "bin:1"
-for my $b (@bins) {
-  my $chrom = $b->seq_id;
-  print join("\t",($chrom,$b->start, $b->end,$b->mean),"\n";
+for my $chrom ( @seq_ids ) {
+    my $len = $wig->length($chrom);
+    my $bincount =int( $len / $windowsize);
+    warn("bincount = $bincount for $chrom:1-$len\n");
+
+    my @bins = $wig->features(
+	-seq_id => $chrom,
+	-type=>"bin:$bincount");
+
+    for my $b (@bins) {
+	print join("\t",$chrom,$b->start, $b->end,$b->mean),"\n";
+    }
 }
