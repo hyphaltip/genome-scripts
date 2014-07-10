8855b59980835b9c15a23ed75985793d2bfec60f
Jason Stajich <jason@bioperl.org>
2014-07-10 13:45:25 -0700

    minor tweaks

--- gene_prediction/gff3_to_cdspep.pl
+++ gene_prediction/gff3_to_cdspep.pl
@@ -123,7 +123,7 @@ for my $genome ( readdir(DIR) ) {
 	my @features = $db->get_features_by_type($t);
 	warn("there are ", scalar @features, " gene features\n") if $verbose;
 	for my $f ( @features ) {
-	    my ($gname) = $f->name || $f->load_id;
+	    my ($gname) = $f->display_id || $f->load_id;
 	    $gout->write_seq(Bio::PrimarySeq->new(-id => "$gname",
 						  -seq=> $f->dna,
 						  -desc => 
@@ -135,7 +135,7 @@ for my $genome ( readdir(DIR) ) {
 	    ### $f->load_id, "\n") if $verbose;
 	    for my $mRNA ( @mrna ) {  
 		my $cds;
-		my ($id) = $mRNA->name || $mRNA->load_id;
+		my ($id) = $mRNA->display_id || $mRNA->load_id;
 
 
 		my $lastexon;
